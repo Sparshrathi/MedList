50db900f88dd11483494fd2f908189ee2fca62f0
Sparsh Rathi <sparshrathi1999@gmail.com>
2022-01-11 15:00:04 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView
+} from 'react-native';
+
+
+const App =(props)=> {
+  return(
+   <View>
+      <Text>hello</Text>
+   </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    screen:{
+      flex:1
+    }
+});
+
+export default App;
